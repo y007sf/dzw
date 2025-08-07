d252eb91c74435b79fad455ee18f4f76bd64265e
y007sf <abdumalikovyusuf7@gmail.com>
2025-08-07 13:09:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-var name = prompt("Ismingizni kiriting") 
-var age = +prompt("Yoshingizni kiriting")
-console.log("Mening ismim: " + name);
-console.log("Men " + age + " yoshdaman");
-var misol1 = +prompt("6+3=")
-var misol2 = +prompt("15-4=")
-var misol3 = +prompt("12*4=")
-var misol4 = +prompt("15/3=")
-var misol5 = +prompt("10%3=")
-console.log("Misol 1: 6 + 3 = 9 Sizning javobingiz " + misol1);
-console.log("Misol 2: 15 - 4 = 11 Sizning javobingiz " + misol2);
-console.log("Misol 3: 12 * 4 = 48 Sizning javobingiz " + misol3);
-console.log("Misol 4: 15 / 3 = 5 Sizning javobingiz " + misol4);
-console.log("Misol 5: 10 % 3 = 1 Sizning javobingiz " + misol5);
-alert("Natijani korish uchun konsolni oching")
+let a = +prompt("Birinchi sonni kiriting");
+let b = +prompt("Ikkinchi sonni kiriting");
+let c = +prompt("Uchinchi sonni kiriting");
 
+if (a >= b && a <= c || a <= b && a >= c) {
+    orta = a;
+} else if (b >= a && b <= c || b <= a && b >= c) {
+    orta = b;
+} else {
+    orta = c;
+}
 
-
-
+alert("O'rta qiymat : " + orta);
